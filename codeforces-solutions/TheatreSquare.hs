29a644d1a4c1b7e568ead2577cb2056b628834e3
Stephanie Chan <stephaniechan219@gmail.com>
2016-10-29 18:13:18 -0700

    fixed typing

--- codeforces-solutions/TheatreSquare.hs
+++ codeforces-solutions/TheatreSquare.hs
@@ -1,6 +1,6 @@
 module TheatreSquare where
 
---theatreSquare :: Double -> Double -> Double -> Double
+theatreSquare :: Int -> Int -> Int -> Int
 theatreSquare n m a
   | (mod n a == 0)&&(mod m a == 0) = div n a * div m a
   | (mod n a /= 0)&&(mod m a == 0) = (div n a + 1)* div m a
